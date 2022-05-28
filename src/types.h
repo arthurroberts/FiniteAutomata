e5fc3c48a782e9f65accc7689b8d1897d9f101c3
Arthur Roberts <arthur.roberts@ecs.vuw.ac.nz>
2022-05-29 10:15:10 +1200

    Update types.h

--- src/types.h
+++ src/types.h
@@ -1,1 +1,16 @@
+/ input can be of different types
+// chars or strings say
+//template <typename TI>
+struct InputLetter{
+	char in;
+  //  TI in;
+	InputLetter(){ }
+	InputLetter(char t){in=t;}
+    bool operator == (const InputLetter& rhs) ;
+	void print(){ std::cout<<in; }
+};
+
+ bool InputLetter::operator == (const InputLetter& rhs) {
+    return ( this->in  == rhs.in); 
+ } 
 
